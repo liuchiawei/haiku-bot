059364fab938efa8c8d0210d52f1c251bf9dcbd5
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-08 17:08:52 +0900

    feat/ui: add cursor pointer to shadcn button

--- src/components/ai-elements/conversation.tsx
+++ src/components/ai-elements/conversation.tsx
@@ -46,7 +46,7 @@ export const ConversationScrollButton = ({
     !isAtBottom && (
       <Button
         className={cn(
-          'absolute bottom-4 left-[50%] translate-x-[-50%] rounded-full',
+          'absolute bottom-4 left-1/2 -translate-x-1/2 rounded-full',
           className
         )}
         onClick={handleScrollToBottom}
